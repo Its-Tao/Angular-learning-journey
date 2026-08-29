ed52f1f9380cb6feb8425c87b2f7c407250c3c02
Its-Tao <taofinanaswabu@gmail.com>
2026-08-29 22:17:02 +0300

    components and services are added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import {Navbar} from './components/navbar/navbar';
+import { RouterOutlet } from '@angular/router';
+import { StudentList } from './components/student-list/student-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar],
+  imports: [Navbar, RouterOutlet, StudentList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/student-list/student-list.ts
+++ src/app/components/student-list/student-list.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Student } from '../../models/student';
+import { StudentService } from '../../services/student';
+
 
 @Component({
   selector: 'app-student-list',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './student-list.html',
   styleUrl: './student-list.css',
 })
-export class StudentList {}
+export class StudentList implements OnInit {
+  students: Student[]=[];
+
+  constructor( private studentService:StudentService) {}
+
+  ngOnInit(): void {
+    this.students = this.studentService.getStudents();
+  }
+}
+
